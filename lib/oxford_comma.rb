8117cb66207a9b75a1be46beff296aa4210bbad7
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-16 21:28:45 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,8 @@ def oxford_comma(array)
     return array.join
   elsif array.count == 2
     return array.join(" and ")
+  elsif array.count == 3
+    array.insert(3, " and ")
+    return array.join(", ")
   end
 end
