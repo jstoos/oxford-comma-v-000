b1f14c564b5fcc16e94de628f9330920e2125904
Jennifer Stoos <jstoos@bitlaw.com>
2017-11-16 21:34:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     return array.join
   elsif array.count == 2
     return array.join(" and ")
-  elsif array.count == 3
+  else
     array[-1] = "and #{array[-1]}"
     return array.join(", ")
   end
